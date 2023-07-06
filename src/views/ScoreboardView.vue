b0f53d5a2f5e3db0d3ade86a40ec52ae99153e2c
Aegid1 <107999252+Aegid1@users.noreply.github.com>
2023-07-06 20:45:13 +0200

    small change to scoreboard dependency

--- src/views/ScoreboardView.vue
+++ src/views/ScoreboardView.vue
@@ -1,10 +1,5 @@
 <!-- eslint-disable no-template-curly-in-string -->
 <template>
-  <head>
-    <link rel= "stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-  </head>
-
     <div>
       <h1 class = "title"> Scoreboard </h1>
       <div class = "layout">
