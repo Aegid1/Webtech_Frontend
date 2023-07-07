7dae1ec40f917302feb0b73dbd1e4d0659426272
ProgMojo <m.moussa2422@gmail.com>
2023-07-07 15:45:23 +0200

    Ansatz fpr ProfileView und Aufnahme ins Index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegistrationView from '../views/RegistrationView.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const routes = [
   {
@@ -11,7 +12,7 @@ const routes = [
     props: true
   },
   {
-    path: '/',
+    path: '/login',
     name: 'login',
     component: LoginView
   },
@@ -23,10 +24,12 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/',
+    name: 'ProfileView',
+    component: ProfileView
   }
 ]
 
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="container">
+    <div class="DivWrapper">
+      <div class="firstDiv"></div>
+      <div class="secondDiv">
+        <form @submit.prevent="login">
+          <h1>Login into your Home</h1>
+          <div class="WrapperLoginForm">
+            <div class="loginForm">
+                  <div class="home__container.container.grid.section__border.">
+                    <div class="home__data grid">
+                      <h1 class="home-title">
+                        Hi, I'm Aegid <br>
+                      </h1>
+                      <div class="home_blob grid">
+                        <div clas="div.home_perfil">
+                          <img src="assets/img/prefil.png" alt="home perfil">
+                        </div>
+                        <img src="assets/img/shape-wawes.svg" alt="" class="home__shape-wawes">
+                        <img src="assets/img/shape-circle.svg" alt="" class="home__shape-circle">
+                      </div>
+                    </div>
+                  </div>
+            </div>
+          </div>
+        </form>
+        <DarkModeView :isDarkMode="isDarkMode" @toggleDarkMode="toggleDarkMode" />
+        <button @click="toggleDarkMode">Toggle Dark Mode</button>
+      </div>
+      <div class="thirdDiv"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileView.vue'
+}
+</script>
+
+<style scoped>
+
+.firstDiv {
+  height: 100vh;
+  width: 15%;
+  z-index: 0;
+  border-radius: 0px 30px 0px 0px;
+  background-color: #20c9c1;
+  float: left;
+}
+
+.secondDiv{
+  width: 60%;
+  height: 0px;
+  z-index: 0;
+  margin: 0 auto;
+}
+
+.thirdDiv{
+  width: 15%;
+  height: 100vh;
+  z-index: 0;
+  border-radius: 30px 0px 0px 0px;
+  background-color: #20c9c1;
+  float: right;
+}
+
+.WrapperLoginForm {
+  background-color: #20c9c1;
+  padding-top: 30px;
+  border-radius: 30px 30px 30px 30px;
+}
+
+.form-text{
+  margin: 10px;
+  text-align: left;
+  text-align: left;
+}
+
+.form-check-input{
+  background-color: #2c3e50;
+}
+
+.loginForm{
+  text-align: left;
+  padding-left: 20px;
+}
+
+.form-check-input{
+  margin: auto;
+}
+.LoginButton{
+  background-color: White;
+  max-width: 350px;
+  margin-top: 10px;
+  padding: 15px;
+  padding-left: 30px;
+  padding-right: 30px;
+  border-radius: 15px 15px 15px 15px;
+}
+
+.InputFields{
+  padding: 1vh;
+}
+
+.InputFields{
+  width: 100vh;
+  background-color: white;
+  display: inline-block;
+}
+
+.ForgotPassword{
+  padding-top: 10px;
+  text-align: left;
+  padding-left: 20px;
+}
+.Checkbox{
+  text-align: left;
+  padding-top: 3px;
+  padding-left: 20px;
+}
+.LoginButtonDiv{
+  text-align: left;
+  padding-left: 20px;
+  padding-bottom: 30px;
+}
+</style>
